68459a759de37629e0c9662ae25554ca26c0b748
Filipe Cabecinhas <github@filcab.net>
2020-04-02 15:14:05 +0100

    shell: Merge uname -s blocks
    Cleaned up uname -s to minimize number of executions
    Drive-by: bump vim version for Windows aliases

--- .rc.0-defaults.sh
+++ .rc.0-defaults.sh
@@ -4,15 +4,6 @@ source ~/.paths
 alias cd..='cd ..'
 alias less='less -r'
 alias grep='grep --color=auto'
-case $(uname -s) in
-  Darwin)
-    alias ls='ls -FG'
-    alias ldd='otool -L'
-    ;;
-  linux|MINGW*)
-    alias ls="ls -F --color"
-    ;;
-esac
 
 ### Vim and $EDITOR
 if [ -z "$EDITOR" ] && hash vim &> /dev/null; then
@@ -26,23 +17,43 @@ fi
 
 case $(uname -s) in
   Darwin)
+    alias ls='ls -FG'
+    alias ldd='otool -L'
+
     # Have we brewed a vim?
     if [ -f ~/dev/brew/bin/vim -a -x ~/dev/brew/bin/vim ]; then
       export EDITOR=~/dev/brew/bin/vim
     fi
+
+    ### Homebrew settings
+    # Always build from source
+    export HOMEBREW_BUILD_FROM_SOURCE=1
+    # No analytics
+    export HOMEBREW_NO_ANALYTICS=1
+
+    ### Disable the creepy, unacceptable, stats requests by CocoaPods
+    export COCOAPODS_DISABLE_STATS=1
+    ;;
+
+  linux)
+    alias ls="ls -F --color"
     ;;
   MINGW*)
+    alias ls="ls -F --color"
     # Have we installed a more current vim? (We're assuming we have msys git, which has an old vim)
-    if [ -f /c/Program\ Files/Vim/vim81/vim.exe ]; then
+    if [ -f /c/Program\ Files/Vim/vim82/vim.exe ]; then
       # Also alias vim/gvim so we get the updated version in most places
-      alias vim="winpty /c/Program\ Files/Vim/vim81/vim.exe"
-      alias view="winpty /c/Program\ Files/Vim/vim81/view.exe"
+      alias vim="winpty /c/Program\ Files/Vim/vim82/vim.exe"
+      alias view="winpty /c/Program\ Files/Vim/vim82/view.exe"
       alias vimdiff="vim -d"
-      alias gvim="/c/Program\ Files/Vim/vim81/gvim.exe"
-      alias gview="winpty /c/Program\ Files/Vim/vim81/gview.exe"
+      alias gvim="/c/Program\ Files/Vim/vim82/gvim.exe"
+      alias gview="winpty /c/Program\ Files/Vim/vim82/gview.exe"
       alias gvimdiff="gvim -d"
-      export EDITOR="winpty /c/Program\ Files/Vim/vim81/vim.exe"
+      export EDITOR="winpty /c/Program\ Files/Vim/vim82/vim.exe"
     fi
+
+    # Use native symlinks on Windows by default. Assumes we have perms for that, which needs to be done once per machine
+    export MSYS=winsymlinks:nativestrict
     ;;
 esac
 
@@ -59,13 +70,6 @@ export CLICOLOR=1
 
 # So ctest shows us the output if a test failed
 export CTEST_OUTPUT_ON_FAILURE=1
-
-# Use native symlinks on Windows by default. Assumes we have perms for that, which needs to be done once per machine
-case $(uname -s) in
-  MINGW*)
-    export MSYS=winsymlinks:nativestrict
-    ;;
-esac
 
 # Color for manpages:
 export LESS_TERMCAP_mb=$'\E[01;31m'
@@ -86,7 +90,8 @@ function maybe_start_ssh_agent {
       return
   fi
 
-  local env="$HOME/.ssh/agent.env.$(uname -s)"
+  local uname_s="$(uname -s)"
+  local env="$HOME/.ssh/agent.env.${uname_s}"
 
   agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
 
@@ -106,7 +111,7 @@ function maybe_start_ssh_agent {
   fi
 
   if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ] || [ $agent_run_state = 1 ]; then
-      [ "$(uname -s)" = "Darwin" ] && ssh-add -A
+      [ "${uname_s}" = "Darwin" ] && ssh-add -A
       ssh-add
   fi
 }
@@ -129,15 +134,6 @@ fi
 
 ### Ninja status: "time [left/running/finished]"
 export NINJA_STATUS="$(tput bold)%e$(tput sgr0) [$(tput bold)$(tput setaf 1)%u$(tput sgr0)/$(tput bold)$(tput setaf 3)%r$(tput sgr0)/$(tput bold)$(tput setaf 2)%f$(tput sgr0)] "
-
-### Homebrew settings
-# Always build from source
-export HOMEBREW_BUILD_FROM_SOURCE=1
-# No analytics
-export HOMEBREW_NO_ANALYTICS=1
-
-### Disable the creepy, unacceptable, stats requests by CocoaPods
-export COCOAPODS_DISABLE_STATS=1
 
 case "$SHELL" in
   *bash|*bash.exe)
