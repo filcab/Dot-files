90981c1c04020d27850fec3101e4feebbed27196
Filipe Cabecinhas <github@filcab.net>
2021-01-25 21:49:24 +0000

    windows/timeit: Use a shell to execute

--- .windows/bin/timeit.bat
+++ .windows/bin/timeit.bat
@@ -8,4 +8,6 @@
 @rem endlocal
 
 @rem new version: just rely on python
-python -c "import subprocess;import sys;import time;start=time.time();ret=subprocess.run(sys.argv[1:]).returncode;taken=time.time()-start;mins=int(taken//60);secs=taken%%60;print(f'\nreal:\t{mins}m{secs:.3}s');sys.exit(ret)" %*
+@rem Use shell=yes to call the subprocess as that's what's expected. Otherwise we'd need to do some shell-related work
+@rem TODO: Double-check if this means we expand stuff twice on Windows. IIRC, it's on the called process to expand the arguments, not on the shell
+python -c "import subprocess;import sys;import time;start=time.time();ret=subprocess.run(sys.argv[1:], shell=True).returncode;taken=time.time()-start;mins=int(taken//60);secs=taken%%60;print(f'\nreal:\t{mins}m{secs:.3}s');sys.exit(ret)" %*
