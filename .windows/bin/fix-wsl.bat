56b5124db8d7ada91934b81cf72039b6836ea143
Filipe Cabecinhas <github@filcab.net>
2024-10-14 14:53:48 +0100

    fix-wsl: trim the number of thigs to restart, just do the vmcompute service

--- .windows/bin/fix-wsl.bat
+++ .windows/bin/fix-wsl.bat
@@ -1,1 +1,2 @@
-powershell -Command "Start-Process netsh -ArgumentList winsock,reset -verb runAs ; Start-Process sc -ArgumentList stop, vmcompute -verb runAs; Start-Process sc -ArgumentList start,vmcompute -verb runAs"
+REM powershell -Command "Start-Process netsh -ArgumentList winsock,reset -verb runAs ; Start-Process sc -ArgumentList stop, vmcompute -verb runAs; Start-Process sc -ArgumentList start,vmcompute -verb runAs"
+powershell -Command "Start-Process sc -ArgumentList stop, vmcompute -verb runAs; Start-Process sc -ArgumentList start,vmcompute -verb runAs"
