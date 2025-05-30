7a5108f7cc347a2eb855570b0a013accbad7ceef
Filipe Cabecinhas <github@filcab.net>
2025-05-30 15:37:42 +0100

    fix-wsl: only stop the vmcompute service. Let it startup up on its own next time we use WSL

--- .windows/bin/fix-wsl.bat
+++ .windows/bin/fix-wsl.bat
@@ -1,2 +1,8 @@
-REM powershell -Command "Start-Process netsh -ArgumentList winsock,reset -verb runAs ; Start-Process sc -ArgumentList stop, vmcompute -verb runAs; Start-Process sc -ArgumentList start,vmcompute -verb runAs"
-powershell -Command "Start-Process sc -ArgumentList stop, vmcompute -verb runAs; Start-Process sc -ArgumentList start,vmcompute -verb runAs"
+@echo pwsh fix-wsl
+
+@REM powershell -Command "Start-Process netsh -ArgumentList winsock,reset -verb runAs ; Start-Process sc -ArgumentList stop, vmcompute -verb runAs; Start-Process sc -ArgumentList start,vmcompute -verb runAs"
+@REM powershell -Command "Start-Process sc -ArgumentList stop, vmcompute -verb runAs; Start-Process sc -ArgumentList start,vmcompute -verb runAs"
+
+@REM powershell -Command { Start-Process -verb runAs powershell "-Command Restart-Service vmcompute -Force" \}
+
+powershell -Command "Start-Process -verb runAs sc -ArgumentList stop,vmcompute"
